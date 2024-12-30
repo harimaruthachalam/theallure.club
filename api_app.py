cabde33b1b7ed8079777ecd9e3f9442aa1ae229a
Srihari Maruthachalam <sriharimaruthachalam@Sriharis-MacBook-Air.local>
2024-12-30 14:28:32 +0530

    Progress

--- api_app.py
+++ api_app.py
@@ -5,6 +5,10 @@ app=FastAPI()
 async def root():
     return {"message":"Hello World"}
 
+@app.post("/")
+async def root():
+    return {"message":"Hello World"}
+
 @app.get("/test")
 async def root():
     return {"message":"this is test"}
