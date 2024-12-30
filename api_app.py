8338eed4c03705a707c552097279f4ccea07e9da
Srihari Maruthachalam <sriharimaruthachalam@Sriharis-MacBook-Air.local>
2024-12-30 14:48:15 +0530

    Minor

--- api_app.py
+++ api_app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import ollama
 
 class IncomingPayload(BaseModel):
     text: str
@@ -13,7 +14,25 @@ async def root():
 
 @app.post("/")
 async def root(payload: IncomingPayload):
-    return {"message":f"Hello World {payload.text}"}
+    data = payload.text
+    response = ollama.chat(model='ben1t0/tiny-llm:latest', messages=[
+    {
+        'role': 'system',
+        'content': 'you are a poet',
+    },
+    {
+        'role': 'user',
+        'content': 'limit to 50 words',
+    },
+    {
+        'role': 'user',
+        'content': data,
+    },
+    ])
+
+    response['message']['content']
+
+    return {"message":f"{payload.text}"}
 
 @app.get("/test")
 async def root():
